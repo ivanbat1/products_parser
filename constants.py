f30c94750e3c7024324a843cb34016e49450894b
Ivan <baturin.ivan9@gmail.com>
2021-08-05 12:16:15 +0300

    add constants file

---
+++ constants.py
@@ -0,0 +1,13 @@
+VERSION_API = "VERION API"
+URL_NAME = "URL TO CATALOG"
+XLSX_FILE_NAME = "FILE NAME"
+SHEET_NAME = "SHEET NAME"
+AUTH = ('LOGIN', 'PASS')
+API_KEY = "YOUR API KEY"
+IMAGES_FILE_PATH = 'PATH TO IMAGE FILE'
+DEBUG = True  # for developers
+
+try:
+    import local_constants
+except ImportError:
+    pass
